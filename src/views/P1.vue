797241ded53bebb260d92c55754b0196508291b9
gun <gungungggun@gmail.com>
2018-10-11 18:33:35 +0900

    ...

--- src/views/P1.vue
+++ src/views/P1.vue
@@ -6,6 +6,10 @@
       data-separator-vertical="\n>>>$"
       data-separator-notes="^Note:"
     )
+    section
+      h1 aaa
+    section
+      h1 bbb
 </template>
 
 <script lang="coffee">
@@ -13,8 +17,11 @@ import Reveal from 'reveal.js/js/reveal'
 
 export default
   methods:
+    slide5: () ->
+      alert()
     slidechanged: (e) ->
-      console.log(e.indexh)
+      if e.indexh == 5
+        this.slide5()
   mounted: () ->
     Reveal.addEventListener('slidechanged', this.slidechanged)
 </script>
