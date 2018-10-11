82da6c050ee562267ea5bfd8ecdc2f7ea5e79294
gun <gungungggun@gmail.com>
2018-10-11 20:22:08 +0900

    chartjs

--- src/views/P1.vue
+++ src/views/P1.vue
@@ -9,16 +9,36 @@
     section
       h1 aaa
     section
-      h1 bbb
+      doughnut-chart(:chart-data="chartData" :options="chartOptions" :width="400"
+  :height="200")
+      button(@click="push") push
 </template>
 
 <script lang="coffee">
 import Reveal from 'reveal.js/js/reveal'
+import DoughnutChart from '@/components/DoughnutChart.js'
 
 export default
+  components:
+    DoughnutChart: DoughnutChart
+  data: () ->
+    count: 1
+    chartData: null
+    chartOptions: {}
   methods:
+    push: () ->
+      this.chartData = {
+        labels: ['Yes', 'No']
+        datasets: [
+          {
+            backgroundColor: ['#00b', '#b00']
+            borderColor: ['#00b', '#b00']
+            data: [this.count, 2]
+          }
+        ]
+      }
+      this.count++
     slide5: () ->
-      alert()
     slidechanged: (e) ->
       if e.indexh == 5
         this.slide5()
